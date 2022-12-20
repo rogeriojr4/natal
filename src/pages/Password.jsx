64ba63304e02e29ec9120f72e01de916f2467a07
Roger <rogerio@ae.studio>
2022-12-20 01:02:29 -0300

    better loading

--- src/pages/Password.jsx
+++ src/pages/Password.jsx
@@ -1,15 +1,16 @@
-import Nullstack from 'nullstack'
+import Nullstack from "nullstack";
 
-import LoadingScreen from './LoadingScreen'
+import LoadingScreen from "./LoadingScreen";
 
 class Password extends Nullstack {
+  hasPassword = null;
+  user = null;
 
-  hasPassword = null
-  user = null
+  password = "";
 
-  password = ''
+  error = "";
 
-  error = ''
+  loading = false;
 
   static async checkLogin({ db, password, userId }) {
     const user = await db.user.findUnique({
@@ -19,16 +20,14 @@ class Password extends Nullstack {
       include: {
         WhoToSend: 1,
       },
-    })
-
-    console.log(user.password)
+    });
 
     if (user.password) {
       if (password === user.password) {
-        delete user.password
-        return user
+        delete user.password;
+        return user;
       }
-      return { error: 'Senha incorreta :(' }
+      return { error: "Senha incorreta :(" };
     }
     // Update user's password
     await db.user.update({
@@ -38,8 +37,8 @@ class Password extends Nullstack {
       where: {
         id: parseInt(userId),
       },
-    })
-    return user
+    });
+    return user;
   }
 
   static async getUser({ db, userId }) {
@@ -47,30 +46,32 @@ class Password extends Nullstack {
       where: {
         id: parseInt(userId),
       },
-    })
+    });
 
-    return { ...user, password: !!user.password }
+    return { ...user, password: !!user.password };
   }
 
   async initiate({ params }) {
-    const user = await this.getUser({ userId: params.id })
-    this.hasPassword = user.password
-    this.user = user
+    const user = await this.getUser({ userId: params.id });
+    this.hasPassword = user.password;
+    this.user = user;
   }
 
   async doLogin(context) {
-    const { params } = context
+    this.loading = true;
+    const { params } = context;
     const response = await this.checkLogin({
       password: this.password,
       userId: params.id,
-    })
+    });
 
     if (!response.error) {
-      context.me = response
-      context.router.path = '/getPrize'
+      context.me = response;
+      context.router.path = "/getPrize";
     } else {
-      this.error = 'Senha errada 😭'
+      this.error = "Senha errada 😭";
     }
+    this.loading = false;
   }
 
   renderLogin() {
@@ -82,21 +83,31 @@ class Password extends Nullstack {
           <br />
           Por favor, insira a senha, se esqueceu, fala com o Rogerinho 😆
         </p>
-        <input bind={this.password} class="w-full px-2 text-xl rounded-md mt-6" type="text" placeholder="Senha" />
-        {this.error && <p class="text-dark-red font-medium">Oh não, parece que você errou a senha, tente de novo!</p>}
+        <input
+          bind={this.password}
+          class="w-full px-2 text-xl rounded-md mt-6"
+          type="text"
+          placeholder="Senha"
+        />
+        {this.error && (
+          <p class="text-dark-red font-medium">
+            Oh não, parece que você errou a senha, tente de novo!
+          </p>
+        )}
         <button onclick={this.doLogin} class="w-full mt-4">
           Entrar
         </button>
       </div>
-    )
+    );
   }
 
   renderCreatePassword() {
     return (
       <>
         <p class="text-2xl text-white">
-          Oi {this.user.name}!! <br /> <br /> Quase lá! Antes de tudo, vamos criar uma senha? <br /> <br /> Só para o
-          caso <s>da madrinha</s> de alguém curioso querer ver o seu amigo secreto 🤓
+          Oi {this.user.name}!! <br /> <br /> Quase lá! Antes de tudo, vamos
+          criar uma senha? <br /> <br /> Só para o caso <s>da madrinha</s> de
+          alguém curioso querer ver o seu amigo secreto 🤓
           <br />
           <br />
           <span class="font-normal">
@@ -114,13 +125,13 @@ class Password extends Nullstack {
           Entrar
         </button>
       </>
-    )
+    );
   }
 
   render() {
     return (
       <>
-        {this.hasPassword === null ? (
+        {this.hasPassword === null || this.loading ? (
           <LoadingScreen />
         ) : (
           <section class="h-screen w-full flex items-center justify-center">
@@ -130,9 +141,8 @@ class Password extends Nullstack {
           </section>
         )}
       </>
-    )
+    );
   }
-
 }
 
-export default Password
+export default Password;
